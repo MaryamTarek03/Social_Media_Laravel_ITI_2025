4eb48d4a93c5ff4470610b024b19ec13ea31eab9
Mariam Sanad <mariemsanad48@gamil.com>
2025-09-09 19:04:51 +0300

    Descriptive commit message

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+        public function store(){
+        //get the user data
+        $data = Request()->all();
+        return $data;
+        //store the user data in database
+
+        //redirection to post.index
+        return to_route('posts.index');
+    }
 }
