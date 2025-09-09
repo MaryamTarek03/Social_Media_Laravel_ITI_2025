b928fea12bb0bdcb2bd5cfb6e0c90a52b3c87d51
MaryamTarek03 <mariamtarek2233@gmail.com>
2025-09-09 22:44:38 +0300

    added users into navigation for easier debugging

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::paginate(10);
+        $users = User::paginate(30);
         return view('users.index', compact('users'));
     }
 
